b18623093687e3cba9400668167a40979297205a
mardiaz353 <adiaz064@gmail.com>
2016-09-03 15:04:27 -0700

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,185 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btn3_1 = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblMoves = New System.Windows.Forms.Label()
+        Me.btn2_1 = New System.Windows.Forms.Button()
+        Me.btn1_1 = New System.Windows.Forms.Button()
+        Me.btn1_2 = New System.Windows.Forms.Button()
+        Me.btn2_2 = New System.Windows.Forms.Button()
+        Me.btn3_2 = New System.Windows.Forms.Button()
+        Me.btn1_3 = New System.Windows.Forms.Button()
+        Me.btn2_3 = New System.Windows.Forms.Button()
+        Me.btn3_3 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btn3_1
+        '
+        Me.btn3_1.BackColor = System.Drawing.Color.Red
+        Me.btn3_1.Location = New System.Drawing.Point(13, 13)
+        Me.btn3_1.Name = "btn3_1"
+        Me.btn3_1.Size = New System.Drawing.Size(70, 70)
+        Me.btn3_1.TabIndex = 0
+        Me.btn3_1.UseVisualStyleBackColor = False
+        '
+        'btnReset
+        '
+        Me.btnReset.Location = New System.Drawing.Point(12, 241)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(223, 37)
+        Me.btnReset.TabIndex = 1
+        Me.btnReset.Text = "Reset"
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(26, 291)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(59, 20)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Moves:"
+        '
+        'lblMoves
+        '
+        Me.lblMoves.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblMoves.Location = New System.Drawing.Point(91, 288)
+        Me.lblMoves.Name = "lblMoves"
+        Me.lblMoves.Size = New System.Drawing.Size(133, 27)
+        Me.lblMoves.TabIndex = 3
+        Me.lblMoves.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'btn2_1
+        '
+        Me.btn2_1.BackColor = System.Drawing.Color.Red
+        Me.btn2_1.Location = New System.Drawing.Point(13, 89)
+        Me.btn2_1.Name = "btn2_1"
+        Me.btn2_1.Size = New System.Drawing.Size(70, 70)
+        Me.btn2_1.TabIndex = 4
+        Me.btn2_1.UseVisualStyleBackColor = False
+        '
+        'btn1_1
+        '
+        Me.btn1_1.BackColor = System.Drawing.Color.Red
+        Me.btn1_1.Location = New System.Drawing.Point(13, 165)
+        Me.btn1_1.Name = "btn1_1"
+        Me.btn1_1.Size = New System.Drawing.Size(70, 70)
+        Me.btn1_1.TabIndex = 5
+        Me.btn1_1.UseVisualStyleBackColor = False
+        '
+        'btn1_2
+        '
+        Me.btn1_2.BackColor = System.Drawing.Color.Red
+        Me.btn1_2.Location = New System.Drawing.Point(89, 165)
+        Me.btn1_2.Name = "btn1_2"
+        Me.btn1_2.Size = New System.Drawing.Size(70, 70)
+        Me.btn1_2.TabIndex = 8
+        Me.btn1_2.UseVisualStyleBackColor = False
+        '
+        'btn2_2
+        '
+        Me.btn2_2.BackColor = System.Drawing.Color.Red
+        Me.btn2_2.Location = New System.Drawing.Point(89, 89)
+        Me.btn2_2.Name = "btn2_2"
+        Me.btn2_2.Size = New System.Drawing.Size(70, 70)
+        Me.btn2_2.TabIndex = 7
+        Me.btn2_2.UseVisualStyleBackColor = False
+        '
+        'btn3_2
+        '
+        Me.btn3_2.BackColor = System.Drawing.Color.Red
+        Me.btn3_2.Location = New System.Drawing.Point(89, 13)
+        Me.btn3_2.Name = "btn3_2"
+        Me.btn3_2.Size = New System.Drawing.Size(70, 70)
+        Me.btn3_2.TabIndex = 6
+        Me.btn3_2.UseVisualStyleBackColor = False
+        '
+        'btn1_3
+        '
+        Me.btn1_3.BackColor = System.Drawing.Color.Red
+        Me.btn1_3.Location = New System.Drawing.Point(165, 165)
+        Me.btn1_3.Name = "btn1_3"
+        Me.btn1_3.Size = New System.Drawing.Size(70, 70)
+        Me.btn1_3.TabIndex = 11
+        Me.btn1_3.UseVisualStyleBackColor = False
+        '
+        'btn2_3
+        '
+        Me.btn2_3.BackColor = System.Drawing.Color.Red
+        Me.btn2_3.Location = New System.Drawing.Point(165, 89)
+        Me.btn2_3.Name = "btn2_3"
+        Me.btn2_3.Size = New System.Drawing.Size(70, 70)
+        Me.btn2_3.TabIndex = 10
+        Me.btn2_3.UseVisualStyleBackColor = False
+        '
+        'btn3_3
+        '
+        Me.btn3_3.BackColor = System.Drawing.Color.Red
+        Me.btn3_3.Location = New System.Drawing.Point(165, 13)
+        Me.btn3_3.Name = "btn3_3"
+        Me.btn3_3.Size = New System.Drawing.Size(70, 70)
+        Me.btn3_3.TabIndex = 9
+        Me.btn3_3.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(249, 341)
+        Me.Controls.Add(Me.btn1_3)
+        Me.Controls.Add(Me.btn2_3)
+        Me.Controls.Add(Me.btn3_3)
+        Me.Controls.Add(Me.btn1_2)
+        Me.Controls.Add(Me.btn2_2)
+        Me.Controls.Add(Me.btn3_2)
+        Me.Controls.Add(Me.btn1_1)
+        Me.Controls.Add(Me.btn2_1)
+        Me.Controls.Add(Me.lblMoves)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.btn3_1)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Blackout - 2D Array"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btn3_1 As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lblMoves As Label
+    Friend WithEvents btn2_1 As Button
+    Friend WithEvents btn1_1 As Button
+    Friend WithEvents btn1_2 As Button
+    Friend WithEvents btn2_2 As Button
+    Friend WithEvents btn3_2 As Button
+    Friend WithEvents btn1_3 As Button
+    Friend WithEvents btn2_3 As Button
+    Friend WithEvents btn3_3 As Button
+End Class
